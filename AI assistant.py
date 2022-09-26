0593ec4bebc80bd8abc4c53c329d7ada1caf6fc5
Shekhar852 <113285674+Shekhar852@users.noreply.github.com>
2022-09-26 15:37:50 +0530

    Update AI assistant.py

--- AI assistant.py
+++ AI assistant.py
@@ -7,7 +7,6 @@ import os
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-#print(voices[1].id)
 engine.setProperty('voice', voices[1].id)
 
 def speak(audio):
@@ -37,7 +36,7 @@ def takecommand():
         query=r.recognize_google(audio, language='en-in')
         print(f"User said: {query}\n")
     except Exception as e:
-        #print(e)
+      
         print("Say that again please....")
         return "None"
     return query    
